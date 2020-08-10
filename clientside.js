73ff7a2d8c1e95d580f9fc6311e6e4482b4d4863
Lucas Hocker <lucas.hocker@dynatrace.com>
2020-08-09 20:16:59 -0500

    large refactoring for stability & performance, series name in tooltip

---
+++ clientside.js
@@ -0,0 +1,364 @@
+var DashboardPowerups = (function () {
+    const TITLE_SELECTOR = '[uitestid="gwt-debug-title"]';
+    const VAL_SELECTOR = '[uitestid="gwt-debug-custom-chart-single-value-formatted-value"] > span:first-of-type';
+    const TILE_SELECTOR = '.grid-tile';
+    const LEGEND_SELECTOR = '[uitestid="gwt-debug-legend"]';
+    const SVG_SELECTOR = '[uitestid="gwt-debug-MARKDOWN"] > div:first-child > div:first-child';
+    const BIGNUM_SELECTOR = '[uitestid="gwt-debug-custom-chart-single-value-formatted-value"] span';
+    const COLORHACK = '!colorhack:';
+    const SVGHACK = '!svghack:';
+    const LINKER = '!link=';
+    const MARKERS = [COLORHACK, SVGHACK, LINKER];
+    const SERIES_OPTS = {
+        "animation": true,
+        "allowPointSelect": true,
+        "enableMouseTracking": true,
+        "states": {
+            "hover": {
+                "enabled": true,
+                "halo": {
+                    "opacity": 0.25,
+                    "size": 10
+                }
+            }
+        },
+
+    };
+    const TOOLTIP_OPTS = {
+        enabled: true,
+        animation: false,
+        outside: true,
+        useHTML: false,
+        hideDelay: 100,
+        shared: true,
+        formatter: function () {
+            return this.points.reduce(function (s, point) {
+                let n = point.series.name;
+                let i = n.indexOf('¦') || "APPLICATION-0000000000000000".length;
+                let sn = n.substring(0, i) || "";
+
+                let $container = $(point.series.chart.container);
+                let $legend = $container.parents(TILE_SELECTOR).find(LEGEND_SELECTOR);
+                if($legend.length){
+                    let series_index = point.series.index;
+                    let series_name = $legend.children(`.gwt-HTML:nth-child(${series_index+1})`).text();
+                    if(series_name.length) sn = series_name;
+                }
+
+                return s + '<br/><span style=\"color:' + point.color + '\">●</span>' + sn + ': ' +
+                    Math.round(point.y, 1) + '';
+            }, '<b>' + Highcharts.dateFormat("%H:%M", this.x) + '</b>');
+        },
+    };
+    const AXIS_OPTS = {
+        crosshair: {
+            color: '#cccccc',
+            width: '1px'
+        }
+    };
+
+    //Private methods
+    const debounce = (func, wait) => {
+        let timeout;
+
+        return function executedFunction(...args) {
+            const later = () => {
+                clearTimeout(timeout);
+                func(...args);
+            };
+
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+        };
+    };
+
+
+
+    //Public methods
+    var pub = {};
+
+    pub.POWERUP_EXT_URL = "";
+
+    pub.hackHighcharts = function () {
+        //be sure not to leak off dashboards
+        if (window.location.hash.startsWith("#dashboard;") ||
+            window.location.hash.startsWith("#dashboard/dashboard;")) {
+            console.log("Powerup: hacking Highcharts...");
+            let hackedCount = 0;
+            let promises = [];
+            Highcharts.charts.slice().forEach(chart => {
+                if (typeof (chart) !== "undefined" &&
+                    !chart.powerupHacked &&
+                    typeof (chart.container) != "undefined") {
+                    let p = new $.Deferred();
+                    promises.push(p);
+                    setTimeout(function () {
+                        if (pub.hackHighchart(chart))
+                            hackedCount++;
+                        p.resolve();
+                    }, 100); //still hitting synchronicity issues, try waiting
+                }
+            });
+            $.when.apply($, promises).then(function () {
+                $(".highcharts-container").css("z-index", 999);
+                console.log("Powerup: " + hackedCount + " Highcharts hacked.");
+                //other dashboard hacking here
+                pub.colorPowerUp();
+                pub.updateSVGPowerUp();
+                pub.cleanMarkup();
+            });
+        } else {
+            console.log("Powerup: no longer on a dashboard, removing hackHighcharts listener...");
+            Highcharts.removeEvent(Highcharts.Chart, 'load', pub.hackHighcharts);
+        }
+    }
+
+    pub.hackHighchart = function (chart) {
+        if (typeof (chart) !== "undefined" &&
+            !chart.powerupHacked &&
+            typeof (chart.container) != "undefined") {
+            chart.series.forEach(series => {
+                series.update(SERIES_OPTS, false);
+            });
+            chart.update({ tooltip: TOOLTIP_OPTS }, false);
+            chart.update({ xAxis: AXIS_OPTS }, false);
+            chart.update({ yAxis: AXIS_OPTS }, false);
+
+
+            chart.redraw(false);
+            //chart.powerupHacked = true;  //Don't know why this doesn't work
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    pub.addHackHighchartsListener = function () {
+        console.log("Powerup: added hackHighcharts listener");
+        Highcharts.addEvent(Highcharts.Chart, 'load', debounce(pub.hackHighcharts,50));
+        pub.hackHighcharts();
+    }
+
+    pub.highlightPointsInOtherCharts = function (e) {
+        const container = e.currentTarget;
+        const charts = Highcharts.charts.filter(x => typeof (x) != "undefined");
+        const chartIndex = charts.findIndex(chart => chart.container === container);
+
+        if (chartIndex > -1) {
+            const chart = charts[chartIndex];
+
+            const event = chart.pointer.normalize(e.originalEvent); // Find coordinates within the chart
+            const point = chart.series[0].searchPoint(event, true); // Get the hovered point
+            //const point = chart.pointer.findNearestKDPoint(
+            //    chart.series, true, e
+            //)
+
+            if (point) {
+                const x = point.x;
+
+                for (let i = 0; i < charts.length; i++) {
+                    if (i != chartIndex) {
+                        for (let s = 0; s < charts[i].series.length; s++) {
+                            const points = charts[i].series[s].points;
+                            for (let p = 0; p < points.length; p++) {
+                                if (points[p].x === x) {
+                                    //points[p].onMouseOver();
+                                    points[p].series.xAxis.drawCrosshair(undefined, points[p]);
+                                    points[p].series.yAxis.drawCrosshair(undefined, points[p]);
+                                    break;
+                                }
+                            }
+                        }
+
+                    } else {
+                        //point.series.xAxis.drawCrosshair(undefined,point);
+                        //point.series.yAxis.drawCrosshair(undefined,point);
+                        try {
+                            //point.series.chart.tooltip.refresh(point,undefined); 
+                        } catch (err) {
+                            //Cannot read property 'category' of undefined
+                            //no idea why or how to stop it, let's just throw it away for now...
+                            //console.log(err.message);
+                            //console.log(point);
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+
+    pub.removeHighlightPointsInOtherCharts = function (e) {
+        const charts = Highcharts.charts.filter(x => typeof (x) != "undefined");
+        for (let i = 0; i < charts.length; i++) {
+            charts[i].xAxis[0].hideCrosshair();
+        }
+    }
+
+    pub.loadChartSync = function () {
+        $('[uitestid="gwt-debug-dashboardGrid"]').on("mouseover", ".highcharts-container", debounce(pub.highlightPointsInOtherCharts, 50));
+        $('[uitestid="gwt-debug-dashboardGrid"]').on("mouseout", ".highcharts-container", pub.removeHighlightPointsInOtherCharts);
+    }
+
+    pub.cleanMarkup = function () {
+        $(TITLE_SELECTOR).each((i, el) => {
+            let $title = $(el);
+            let title = $title.text();
+            let idx = title.length;
+
+            idx = MARKERS.reduce((acc, marker) => (title.includes(marker) ? Math.min(title.indexOf(marker), acc) : idx));
+
+            let newTitle = title.substring(0, idx) +
+                `<span class="powerup-markup">` +
+                title.substring(idx) +
+                `</span>`;
+
+            if (idx < title.length)
+                $title.html(newTitle);
+        });
+    }
+
+    pub.colorPowerUp = function () {
+        $(TITLE_SELECTOR).each((i, el) => {
+            let $title = $(el);
+            let $tile = $title.parents(".grid-tile");
+            let $bignum = $tile.find(BIGNUM_SELECTOR);
+
+            //Step1: change tile colors
+            if ($title.text().includes(COLORHACK)) { //example !COLORHACK:base=high;warn=90;crit=70
+                console.log("Powerup: color hack found");
+                let titletokens = $title.text().split(COLORHACK);
+                let argstring = titletokens[1];
+                let args = argstring.split(";").map(x => x.split("="));
+                if (args.length < 3) {
+                    console.log("Powerup: invalid argstring: " + argstring);
+                    return false;
+                }
+                let base = args.find(x => x[0] == "base")[1];
+                let warn = Number(args.find(x => x[0] == "warn")[1]);
+                let crit = Number(args.find(x => x[0] == "crit")[1]);
+                let val = Number($tile.find(VAL_SELECTOR).text());
+
+                let $target = $bignum; //or $tile
+                $target.removeClass("powerup-colorhack-critical powerup-colorhack-warning powerup-colorhack-normal");
+                if (base == "low") {
+                    if (val < warn) $target.addClass("powerup-colorhack-normal");
+                    else if (val < crit) $target.addClass("powerup-colorhack-warning");
+                    else $target.addClass("powerup-colorhack-critical");
+                } else if (base == "high") {
+                    if (val > warn) $target.addClass("powerup-colorhack-normal");
+                    else if (val > crit) $target.addClass("powerup-colorhack-warning");
+                    else $target.addClass("powerup-colorhack-critical");
+                }
+            }
+        });
+    }
+
+    pub.svgPowerUp = function () {
+        $(SVG_SELECTOR).each((i, el) => {
+            let $svgcontainer = $(el);
+            let $tile = $svgcontainer.parents(".grid-tile");
+
+            if ($svgcontainer.text().includes(SVGHACK)) { //example !SVGHACK:icon=host;link=val1;base=high;warn=90;crit=70 other tile has !link=val1
+                console.log("Powerup: svg hack found");
+                let argstring = $svgcontainer.text().split(SVGHACK)[1];
+
+                let args = argstring.split(";").map(x => x.split("="));
+                let icon = args.find(x => x[0] == "icon")[1];
+                let link = args.find(x => x[0] == "link")[1];
+                let base = args.find(x => x[0] == "base")[1];
+                let warn = Number(args.find(x => x[0] == "warn")[1]);
+                let crit = Number(args.find(x => x[0] == "crit")[1]);
+                let argObj = {
+                    icon: icon,
+                    link: link,
+                    base: base,
+                    warn: warn,
+                    crit: crit
+                }
+                let val = pub.findLinkedVal(link);
+
+                //swap in the svg
+                var imgURL = `${pub.POWERUP_EXT_URL}3rdParty/barista-icons/${icon}.svg`;
+                fetch(imgURL)
+                    .then((response) => response.text())
+                    .then((svgtext) => {
+                        $svgcontainer.empty();
+                        let $svg = $(svgtext)
+                            .attr("data-args", JSON.stringify(argObj))
+                            .appendTo($svgcontainer);
+
+                        $svg.removeClass("powerup-svghack-critical powerup-svghack-warning powerup-svghack-normal");
+                        if (base == "low") {
+                            if (val < warn) $svg.addClass("powerup-svghack-normal");
+                            else if (val < crit) $svg.addClass("powerup-svghack-warning");
+                            else $svg.addClass("powerup-svghack-critical");
+                        } else if (base == "high") {
+                            if (val > warn) $svg.addClass("powerup-svghack-normal");
+                            else if (val > crit) $svg.addClass("powerup-svghack-warning");
+                            else $svg.addClass("powerup-svghack-critical");
+                        }
+                    });
+            }
+        });
+    }
+
+    pub.updateSVGPowerUp = function () {
+        $(SVG_SELECTOR).each((i, el) => {
+            let $svgcontainer = $(el);
+            let $tile = $svgcontainer.parents(".grid-tile");
+            let $svg = $svgcontainer.find("svg:first-of-type");
+
+            if ($svg.length) {
+                let args = $svg.attr("data-args") || "{}";
+                args = JSON.parse(args);
+
+                let val = pub.findLinkedVal(args.link);
+
+                $svg.removeClass("powerup-svghack-critical powerup-svghack-warning powerup-svghack-normal");
+                if (args.base == "low") {
+                    if (val < args.warn) $svg.addClass("powerup-svghack-normal");
+                    else if (val < args.crit) $svg.addClass("powerup-svghack-warning");
+                    else $svg.addClass("powerup-svghack-critical");
+                } else if (args.base == "high") {
+                    if (val > args.warn) $svg.addClass("powerup-svghack-normal");
+                    else if (val > args.crit) $svg.addClass("powerup-svghack-warning");
+                    else $svg.addClass("powerup-svghack-critical");
+                }
+            }
+        });
+    }
+
+    pub.findLinkedVal = function (link) {
+        //find val
+        let link_text = LINKER + link;
+        $(TITLE_SELECTOR).each((i_link, el_link) => {
+            let $linktitle = $(el_link);
+
+            if ($linktitle.text().includes(link_text)) {
+                let $linktile = $linktitle.parents(".grid-tile");
+                val = Number($linktile.find(VAL_SELECTOR).text());
+            }
+        });
+        if (typeof val == "undefined") {
+            console.log("Powerup: unable to match link: " + link_text);
+            return undefined;
+        } else {
+            return val;
+        }
+    }
+
+    pub.addToolTips = function () {
+        if (typeof (pub.addHackHighchartsListener) == "undefined") {
+            console.log("Powerup: clientside.js not loaded yet");
+            setTimeout(pub.initHackHighcharts, 200);
+        } else {
+            pub.addHackHighchartsListener();
+            pub.loadChartSync();
+        }
+    }
+
+
+    return pub;
+})();
---
+++ extside.js
@@ -0,0 +1,119 @@
+console.log("Powerup: Dashboard powerups installed.");
+window.jQuery || console.log("Powerup: No jQuery...");
+var PowerupHacking = false;
+$(document).ready(function () {
+    console.log("Powerup: document ready");
+    $(window).on("load hashchange", powerupListener);
+    console.log("Powerup: listener loaded");
+});
+
+const title_selector = '[uitestid="gwt-debug-title"]';
+const val_selector = '[uitestid="gwt-debug-custom-chart-single-value-formatted-value"] > span:first-of-type';
+const colorize_selector = '.grid-tile';
+const svg_selector = '[uitestid="gwt-debug-MARKDOWN"] > div:first-child > div:first-child';
+const bignum_selector = '[uitestid="gwt-debug-custom-chart-single-value-formatted-value"] span';
+
+const colorhack = '!colorhack:';
+const svghack = '!svghack:';
+const linker = '!link=';
+const markers = [colorhack, svghack, linker];
+const ext_url = chrome.runtime.getURL("");
+
+//Function to check to see if we should do some hacking
+function powerupListener() {
+    if (window.location.hash.startsWith("#dashboard;") ||
+        window.location.hash.startsWith("#dashboard/dashboard;")) {
+        console.log("Powerup: on a dashboard, get to hacking");
+        hackDashboards();
+    } else {
+        console.log("Powerup: not a dashboard, quit.");
+        return;
+    }
+}
+
+
+//This is a function that runs when on the "dashboard" page (the check if we are on that page is at the bottom) 
+function hackDashboards() {
+    PowerupHacking = true;
+
+    //Wait for the dashboard page to load before proceeding 
+    if (document.readyState == 'complete' &&
+        $('[uitestid="gwt-debug-dashboardGrid"]').length &&     //grid is loaded
+        !$(".loader").length &&                                 //main loading distractor gone
+        !$('[uitestid="gwt-debug-tileLoader"]:visible').length  //tile distractors hidden
+    ) {
+        console.log("Powerup: things look ready, begin dashboard hacking...");
+        //Load functions to call in client context
+        injectClientsideLib();
+        injectCSS();
+        console.log("Powerup: clientside libs injected.");
+
+        injectClientsideString(`
+        DashboardPowerups.POWERUP_EXT_URL='${ext_url}';
+        console.log("Powerup: DashboardPowerups=");
+        console.log(DashboardPowerups);
+        console.log("Powerup: --");
+
+        //Step1: color changes
+        DashboardPowerups.colorPowerUp();
+
+        //Step2: swap markdowns for SVGs
+        DashboardPowerups.svgPowerUp();
+
+        //Step3: add tooltips
+        DashboardPowerups.addToolTips();
+
+        //Last Step: cleanup ugly markup
+        DashboardPowerups.cleanMarkup();
+        `);
+
+        console.log("Powerup: hacks complete.");
+        return;
+    } else {
+        console.log("Powerup: doesn't look like things are loaded yet, sleeping 1s.");
+        setTimeout(hackDashboards, 1000);
+    }
+};
+
+function injectClientsideLib() {
+    if (!$("#DashboardPowerupsTag").length) {
+        var $s = $("<script>")
+            .attr("id", "DashboardPowerupsTag")
+            .attr("src", chrome.runtime.getURL("clientside.js")) //execute in webpage context, not extension
+            .appendTo("body");
+    }
+}
+
+function injectClientsideString(s) {
+    let id = uniqId();
+    let wrapped = `
+    function injectedFunction${id}(){
+        if(typeof(DashboardPowerups) == "object"){
+            ${s}
+        } else {
+            console.log("Powerup: client lib not yet loaded.");
+            setTimeout(injectedFunction${id},50);
+        }
+    }
+    injectedFunction${id}();`
+    var $s = $("<script>")
+        .append(document.createTextNode(wrapped))
+        .appendTo("body");
+}
+
+var uniqId = (function () {
+    //usage: let myId = uniqId();
+    var i = 0;
+    return function () {
+      return i++;
+    }
+  })();
+
+function injectCSS(){
+    var $link = $("link")
+    .attr("id","PowerUpCSS")
+    .attr("rel","stylesheet")
+    .attr("type","text/css")
+    .attr("href",ext_url+"powerup.css")
+    .appendTo("head");
+}
