58abdaa47e4704cceb6fdd8518bc1d4e929f129f
Lucas Hocker <lucas.hocker@dynatrace.com>
2020-11-10 18:48:38 -0600

    syntax error

--- extside.js
+++ extside.js
@@ -228,11 +228,11 @@ if (typeof (INJECTED) == "undefined") {
             injectHighchartsModule("heatmap");
             injectClientsideString(`
             //Highcharts Heatmap bug workaround
-            if(Highcharts && Highcharts.)
-            Highcharts.charts
-                .filter(x=>typeof(x)!=="undefined")
-                .filter(x=>typeof(x.colorAxis)=="undefined")
-                .forEach(x=>{x.colorAxis=[];});
+            if(Highcharts && Highcharts.charts)
+                Highcharts.charts
+                    .filter(x=>typeof(x)!=="undefined")
+                    .filter(x=>typeof(x.colorAxis)=="undefined")
+                    .forEach(x=>{x.colorAxis=[];});
             `);
         }
         if (config.Powerups.sankeyPU) {
