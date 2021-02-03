c4961ad0693fef2e2ae8c580aaf994d9a77fbe4a
Lucas Hocker <lucas.hocker@dynatrace.com>
2021-02-02 20:53:18 -0600

    heatmap

--- clientside.js
+++ clientside.js
@@ -2328,11 +2328,20 @@ var DashboardPowerups = (function () {
         });
         //Highcharts expects data to be sorted
         newData = newData.sort((a, b) => {
-            if (a[0] === b[0]) {
-                return a[1] - b[1];
+            if(ms && fmt){ //unflip order
+                if (a[0] === b[0]) {
+                    return b[1] - a[1];
+                } else {
+                    return b[0] - a[0];
+                }
             } else {
-                return a[0] - b[0];
+                if (a[0] === b[0]) {
+                    return a[1] - b[1];
+                } else {
+                    return a[0] - b[0];
+                }
             }
+            
         });
         let newSeries = {
             type: 'heatmap',
