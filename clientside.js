05249b584fe81c451c0ee8558535d9b92cdd2327
Lucas Hocker <lucas.hocker@dynatrace.com>
2021-02-18 09:10:01 -0600

    tooltip formatter

--- clientside.js
+++ clientside.js
@@ -82,7 +82,7 @@ var DashboardPowerups = (function () {
             return this.points.reduce(function (s, point) {
                 let n = point.series.name;
                 let i = n.indexOf('¦') || "APPLICATION-0000000000000000".length;
-                let sn = n.substring(0, i) || "";
+                let sn = n.substring(0, i) || n; //worst case use the existing series name
 
                 let $container = $(point.series.chart.container);
                 let color = point.series.color;
