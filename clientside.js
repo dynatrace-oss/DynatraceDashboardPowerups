56fd9847b2374de2bb0c1341c71554d3c0fe5d3b
Lucas Hocker <lucas.hocker@dynatrace.com>
2021-03-08 21:05:13 -0600

    troubleshooting

--- clientside.js
+++ clientside.js
@@ -1811,10 +1811,14 @@ var DashboardPowerups = (function () {
                         } catch (e) { };
                         //row.substr(1, row.length - 2)
                         //    .split(',')
+                        try{
                         arr = arr
                             .map(x => Array.isArray(x) ? x.join(', ') : x)
                             .map(x => x.trim())
                             .map(x => x.replace(re, '/*$1'));//clean up strings
+                        } catch (e){
+                            console.warn(e);
+                        }
                         dataTable[colIdx][rowIdx] = arr; //safe-store the dataTable in case we want to manipulate later
 
                         //TODO: replace colIdx IFs with colNames
