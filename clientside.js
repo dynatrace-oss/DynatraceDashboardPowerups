4336db320334202791c53efb11ddb8c74eb8d232
Lucas Hocker <lucas.hocker@dynatrace.com>
2021-05-05 15:55:42 -0500

    number

--- clientside.js
+++ clientside.js
@@ -2889,7 +2889,7 @@ var DashboardPowerups = (function () {
                             default:
                                 html += `<td></td>`;
                         }
-                        if(session.matchingConversionGoalsCount)
+                        if(Number(session.matchingConversionGoalsCount))
                             html += `<td><img src='${pub.SVGLib() + 'finishflag.svg'}' onload="DashboardPowerups.SVGInject(this)" class='powerup-sankey-icon powerup-icon-white'></td>`;
                         else 
                             html += `<td></td>`;
