858a266a987d9ed13ca27e680b5af00afb688759
Lucas Hocker <lucas.hocker@dynatrace.com>
2021-06-01 17:08:24 -0500

    styling

--- clientside.js
+++ clientside.js
@@ -3658,15 +3658,15 @@ var DashboardPowerups = (function () {
                             <th>Action name</th>
                             <th>Count</th>
                             <th class="powerupSankeyPopupTooltip">
-                                <img src='${pub.SVGLib() + 'dont-watch.svg'}' alt="Hide Action" onload="DashboardPowerups.SVGInject(this)" class='powerup-sankey-icon powerup-icon-white'>
+                                <img src='${pub.SVGLib() + 'dont-watch.svg'}' alt="Hide Action" onload="DashboardPowerups.SVGInject(this)" class='powerup-sankey-icon powerup-icon-lightpurple'>
                                 <span class="powerupSankeyPopupTooltipText">Hide Action</span>
                             </th>
                             <th class="powerupSankeyPopupTooltip">
-                                <img src='${pub.SVGLib() + 'and-not.svg'}' alt="Sessions not including action" onload="DashboardPowerups.SVGInject(this)" class='powerup-sankey-icon powerup-icon-white'>
+                                <img src='${pub.SVGLib() + 'and-not.svg'}' alt="Sessions not including action" onload="DashboardPowerups.SVGInject(this)" class='powerup-sankey-icon powerup-icon-lightpurple'>
                                 <span class="powerupSankeyPopupTooltipText">Sessions not including action</span>
                             </th>
                             <th class="powerupSankeyPopupTooltip">
-                                <img src='${pub.SVGLib() + 'and.svg'}' alt="Sessions including action" onload="DashboardPowerups.SVGInject(this)" class='powerup-sankey-icon powerup-icon-white'>
+                                <img src='${pub.SVGLib() + 'and.svg'}' alt="Sessions including action" onload="DashboardPowerups.SVGInject(this)" class='powerup-sankey-icon powerup-icon-lightpurple'>
                                 <span class="powerupSankeyPopupTooltipText">Sessions including action</span>
                             </th>
                             </tr>
