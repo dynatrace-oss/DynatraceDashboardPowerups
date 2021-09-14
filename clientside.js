f9c572599d52cd044a48daf691bdc5d25cc4bbf6
Lucas Hocker <lucas.hocker@dynatrace.com>
2021-09-14 15:07:05 -0500

    round instead of toFixed

--- clientside.js
+++ clientside.js
@@ -6807,8 +6807,8 @@ var DashboardPowerups = (function () {
                     outputCol($grid, 'Name', keys);
                     outputCol($grid, 'Min', vals.map(x => x.min));
                     outputCol($grid, 'Max', vals.map(x => x.max));
-                    outputCol($grid, 'Avg', vals.map(x => x.avg.toFixed(2)));
-                    outputCol($grid, 'Stdev', vals.map(x => x.stdev.toFixed(2)));
+                    outputCol($grid, 'Avg', vals.map(x => x.avg.round()));
+                    outputCol($grid, 'Stdev', vals.map(x => x.stdev.round()));
                     let numCols = $grid.children().length;
                     $grid.css('grid-template-columns', `repeat(${numCols}, minmax(80px, auto))`)
                 }
