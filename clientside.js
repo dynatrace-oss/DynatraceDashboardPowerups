4a3608e481999eccbb0df769dbb61cae307db209
Lucas Hocker <lucas.hocker@dynatrace.com>
2021-09-16 11:31:45 -0500

    add powerupMath to VAL_SELECTOR

--- clientside.js
+++ clientside.js
@@ -12,7 +12,7 @@ var DashboardPowerups = (function () {
     const GRID_SELECTOR = '.grid-dashboard';
     const VIEWPORT_SELECTOR = '.grid-viewport';
     const TITLE_SELECTOR = '[uitestid="gwt-debug-title"]';
-    const VAL_SELECTOR = '[uitestid="gwt-debug-custom-chart-single-value-formatted-value"] > span:first-of-type, [uitestid="gwt-debug-kpiValue"] > span:first-of-type, [uitestid="gwt-debug-dexp-visualization-single-value"] span:first-of-type, [uitestid="gwt-debug-visualization-single-value"] span:first-of-type, .powerupVlookup h1';
+    const VAL_SELECTOR = '[uitestid="gwt-debug-custom-chart-single-value-formatted-value"] > span:first-of-type, [uitestid="gwt-debug-kpiValue"] > span:first-of-type, [uitestid="gwt-debug-dexp-visualization-single-value"] span:first-of-type, [uitestid="gwt-debug-visualization-single-value"] span:first-of-type, .powerupVlookup h1, .powerupMath h1';
     const TILE_SELECTOR = '.grid-tile';
     const LEGEND_SELECTOR = '[uitestid="gwt-debug-legend"]';
     const MARKDOWN_SELECTOR = '[uitestid="gwt-debug-MARKDOWN"] > div:first-child > div:first-child';
