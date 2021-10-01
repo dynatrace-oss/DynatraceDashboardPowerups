a953339fe8cf614b0a675edc1371b48718d68fb3
Lucas Hocker <lucas.hocker@dynatrace.com>
2021-10-01 11:57:08 -0500

    troubleshooting

--- clientside.js
+++ clientside.js
@@ -1435,7 +1435,7 @@ var DashboardPowerups = (function () {
         Highcharts.chart($newContainer[0], opts, (newChart) => {
             //Step 2 - forecast into the future
             let forecastTitle = `!PU(forecast):alg=Linear;p=${cast};color=${castcolor}`;
-            pub.PUforecast(newChart, forecastTitle);
+            //pub.PUforecast(newChart, forecastTitle);
 
             //Step 3 - add plotline for threshold
             if (limit) {
