75c385203e9b752ef6af634fcef4b9738a104de4
Lucas Hocker <lucas.hocker@dynatrace.com>
2021-11-05 12:51:55 -0500

    don't forget the dot

--- clientside.js
+++ clientside.js
@@ -920,9 +920,8 @@ var DashboardPowerups = (function () {
             case "string":
                 if (a[key].match(/^[0-9]/)
                     && b[key].match(/^[0-9]/)) { //likely number as a string
-                    let a0 = Number(a[key].replace(/[^0-9]*/g, ''));
-                    let b0 = Number(b[key].replace(/[^0-9]*/g, ''));
-                    console.log(`columnSorterAsc: ${a0} - ${b0} = ${a0 - b0}`);
+                    let a0 = Number(a[key].replace(/[^0-9.]*/g, ''));
+                    let b0 = Number(b[key].replace(/[^0-9.]*/g, ''));
                     return a0 - b0;
                 } else { //should be a string
                     return (a[key].toLowerCase() > b[key].toLowerCase() ? -1 : 1);
@@ -939,8 +938,8 @@ var DashboardPowerups = (function () {
             case "string":
                 if (a[key].match(/^[0-9]/)
                     && b[key].match(/^[0-9]/)) { //likely number as a string
-                    let a0 = Number(a[key].replace(/[^0-9]*/g, ''));
-                    let b0 = Number(b[key].replace(/[^0-9]*/g, ''));
+                    let a0 = Number(a[key].replace(/[^0-9.]*/g, ''));
+                    let b0 = Number(b[key].replace(/[^0-9.]*/g, ''));
                     return b0 - a0;
                 } else { //should be a string
                     return (a[key].toLowerCase() < b[key].toLowerCase() ? -1 : 1);
